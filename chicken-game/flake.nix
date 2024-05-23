19ba507d04628a00acf6f027cf3f64da5dad8fda
rambi <botovik41@gmail.com>
2024-05-23 14:17:13 +0500

    Bloatware or something

---
+++ chicken-game/flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: with pkgs {
+
+    gcc
+
+  };
+}
